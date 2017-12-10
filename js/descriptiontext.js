ba80db6b0a73cba4c69aacffa429a0c30e3d6766
Yuehan <yuehanl@umich.edu>
2017-12-09 23:56:25 -0500

    Done

--- js/descriptiontext.js
+++ js/descriptiontext.js
@@ -1,68 +1,116 @@
-const description = {
-	[
-	  {
-	  	"information": {"title": "Cubism (1907-1922)",
-        'text':"Cubism is an early-20th-century art movement which brought European painting and sculpture historically forward toward 20th century Modern art.",
-        "features":[
-                {"title": "Les Desmoiselles d'Avignon (1907)", "artist": "Pablo Picasso", "image": "url"},
-                {"title": "Bottle and Fishes (1910-1912)", "artist": "Georges Braque", "image": "url"}
-            ]}
-	  },
-	  {
-	  	"information": {"title": "Cubism (1907-1922)",
-        'text':"Cubism is an early-20th-century art movement which brought European painting and sculpture historically forward toward 20th century Modern art.",
-        "features":[
-                {"title": "Les Desmoiselles d'Avignon (1907)", "artist": "Pablo Picasso", "image": "url"},
-                {"title": "Bottle and Fishes (1910-1912)", "artist": "Georges Braque", "image": "url"}
-            ]}
-	  }, 
-	  {
-	  	"information": {"title": "Cubism (1907-1922)",
-        'text':"Cubism is an early-20th-century art movement which brought European painting and sculpture historically forward toward 20th century Modern art.",
-        "features":[
-                {"title": "Les Desmoiselles d'Avignon (1907)", "artist": "Pablo Picasso", "image": "url"},
-                {"title": "Bottle and Fishes (1910-1912)", "artist": "Georges Braque", "image": "url"}
-            ]}
-	  }, 
-	  {
-	  	"information": {"title": "Cubism (1907-1922)",
-        'text':"Cubism is an early-20th-century art movement which brought European painting and sculpture historically forward toward 20th century Modern art.",
-        "features":[
-                {"title": "Les Desmoiselles d'Avignon (1907)", "artist": "Pablo Picasso", "image": "url"},
-                {"title": "Bottle and Fishes (1910-1912)", "artist": "Georges Braque", "image": "url"}
-            ]}
-	  }, 
-	  {
-	  	"information": {"title": "Cubism (1907-1922)",
-        'text':"Cubism is an early-20th-century art movement which brought European painting and sculpture historically forward toward 20th century Modern art.",
-        "features":[
-                {"title": "Les Desmoiselles d'Avignon (1907)", "artist": "Pablo Picasso", "image": "url"},
-                {"title": "Bottle and Fishes (1910-1912)", "artist": "Georges Braque", "image": "url"}
-            ]}
-	  }, 
-	  {
-	  	"information": {"title": "Cubism (1907-1922)",
-        'text':"Cubism is an early-20th-century art movement which brought European painting and sculpture historically forward toward 20th century Modern art.",
-        "features":[
-                {"title": "Les Desmoiselles d'Avignon (1907)", "artist": "Pablo Picasso", "image": "url"},
-                {"title": "Bottle and Fishes (1910-1912)", "artist": "Georges Braque", "image": "url"}
-            ]}
-	  }, 
-	  {
-	  	"information": {"title": "Cubism (1907-1922)",
-        'text':"Cubism is an early-20th-century art movement which brought European painting and sculpture historically forward toward 20th century Modern art.",
-        "features":[
-                {"title": "Les Desmoiselles d'Avignon (1907)", "artist": "Pablo Picasso", "image": "url"},
-                {"title": "Bottle and Fishes (1910-1912)", "artist": "Georges Braque", "image": "url"}
-            ]}
-	  }, 
-	  {
-	  	"information": {"title": "Cubism (1907-1922)",
-        'text':"Cubism is an early-20th-century art movement which brought European painting and sculpture historically forward toward 20th century Modern art.",
-        "features":[
-                {"title": "Les Desmoiselles d'Avignon (1907)", "artist": "Pablo Picasso", "image": "url"},
-                {"title": "Bottle and Fishes (1910-1912)", "artist": "Georges Braque", "image": "url"}
-            ]}
-	  },  
-	]
+const description =
+{
+	"data": [{
+		"information": {
+			"title": "Impressionism & Post-Impressionism (1865-1914)",
+			"text": "Impressionism can be considered the first distinctly modern movement in painting.",
+			"features": [{
+				"title": "Sunrise (1874)",
+				"artist": "Claude Monet",
+				"image": "pics/vetheuil-in-the-fog-1879.jpg"
+			}, {
+				"title": "The Starry Night (1899)",
+				"artist": "Vincent van Gogh",
+				"image": "url"
+			}]
+		}
+	}, {
+		"information": {
+			"title": "Cubism (1907-1922)",
+			"text":"Cubism is an early-20th-century art movement which brought European painting and sculpture historically forward toward 20th century Modern art.",
+			"features": [{
+				"title": "Les Desmoiselles d'Avignon (1907)",
+				"artist": 'Pablo Picasso',
+				"image": "url"
+			}, {
+				"title": "Bottle and Fishes (1910-1912)",
+				"artist": "Georges Braque",
+				"image": "url"
+			}]
+		}
+	}, {
+		"information": {
+			"title": 'Bauhaus (1919-1933)',
+			"text": '',
+			"features": [{
+				"title": "Bauhaus building in Dessau, Germany (1919-1925)",
+				"artist": "Walter Gropius",
+				"image": "url"
+			}, {
+				"title": "Composition with Large Red Plane, Yellow, Black, Gray, and Blue (1921)",
+				"artist": "Piet Mondrian",
+				"image": "url"
+			}]
+		}
+	}, {
+		"information": {
+			"title": "Surrealism (1924-1942)",
+			"text": "",
+			"features": [{
+				"title": "The Accommodations of Desire (1929)",
+				"artist": "Salvador Dalí",
+				"image": "url"
+			}, {
+				"title": "",
+				"artist": "",
+				"image": "url"
+			}]
+		}
+	}, {
+		"information": {
+			"title": "Abstract Expressionism (1940-1950)",
+			"text": "",
+			"features": [{
+				"title": "Lavender Mist(1950)",
+				"artist": "Jackson Pollock",
+				"image": "url"
+			}, {
+				"title": "1948 C (1948)",
+				"artist": "Clyfford Still",
+				"image": "url"
+			}]
+		}
+	}, {
+		"information": {
+			"title": "Pop Art(1950-1970)",
+			"text": "",
+			"features": [{
+				"title": "Campbell's Soup Cans (1962)",
+				"artist":"Andy Warhol",
+				"image": "url"
+			}, {
+				"title": "Bunnies (1966)",
+				"artist": "Sigmar Polke",
+				"image": "url"
+			}]
+		}
+	}, {
+		"information": {
+			"title":"Minimalism(1960-1970)",
+			"text": "",
+			"features": [{
+				"title": "Die (1962)",
+				"artist": "Tony Smith",
+				"image": "url"
+			}, {
+				"title": "Die Fahne Hoch! (1959)",
+				"artist": "Frank Stella",
+				"image": "url"
+			}]
+		}
+	}, {
+		"information": {
+			"title": "Photorealism(1970-2000)",
+			"text": "",
+			"features": [{
+				"title": "McDonalds Pickup (1970)",
+				"artist": "Ralph Goings",
+				"image": "url"
+			}, {
+				"title": "The Woman Eating (1971)",
+				"artist":  "Duane Hanson",
+				"image": "url"
+			}]
+		}
+	}]
 }
