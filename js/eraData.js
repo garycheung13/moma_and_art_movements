740b4e192c7a7d9f0d183cc2bd990928440106e0
Yuehan <yuehanl@umich.edu>
2017-12-09 23:20:51 -0500

    fixed all

--- js/eraData.js
+++ js/eraData.js
@@ -439,12 +439,6 @@ const eraData = {
                 }
             ],
             "tag": "Cubism"
-            "information": {"title": "Cubism (1907-1922)",
-            'text':"Cubism is an early-20th-century art movement which brought European painting and sculpture historically forward toward 20th century Modern art.",
-            "features":[
-                {"title": "Les Desmoiselles d'Avignon (1907)", "artist": "Pablo Picasso", "image": "url"},
-                {"title": "Bottle and Fishes (1910-1912)", "artist": "Georges Braque", "image": "url"}
-            ]}
         },
         {
             "classification": [
