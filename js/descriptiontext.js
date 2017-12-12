134e8c81efe19bd2791c47ee34868b79bcd673e0
Yongjia Duan <yongjia@umich.edu>
2017-12-11 19:40:05 -0500

    add json

--- js/descriptiontext.js
+++ js/descriptiontext.js
@@ -6,12 +6,12 @@ const description =
 			"text": "Impressionism can be considered the first distinctly modern movement in painting.",
 			"features": [{
 				"title": "Sunrise (1874)",
-				"artist": "Claude Monet",
+				"artist": " Claude Monet",
 				"image": "pics/vetheuil-in-the-fog-1879.jpg"
 			}, {
 				"title": "The Starry Night (1899)",
-				"artist": "Vincent van Gogh",
-				"image": "url"
+				"artist": " Vincent van Gogh",
+				"image": "pics/Starry Night.jpg"
 			}]
 		}
 	}, {
@@ -20,12 +20,12 @@ const description =
 			"text":"Cubism is an early-20th-century art movement which brought European painting and sculpture historically forward toward 20th century Modern art.",
 			"features": [{
 				"title": "Les Desmoiselles d'Avignon (1907)",
-				"artist": 'Pablo Picasso',
-				"image": "url"
+				"artist": ' Pablo Picasso',
+				"image": "pics/Les Desmoiselles d'Avignon.jpg"
 			}, {
 				"title": "Bottle and Fishes (1910-1912)",
-				"artist": "Georges Braque",
-				"image": "url"
+				"artist": " Georges Braque",
+				"image": "pics/Bottle and Fishes.jpg"
 			}]
 		}
 	}, {
@@ -34,12 +34,12 @@ const description =
 			"text": '',
 			"features": [{
 				"title": "Bauhaus building in Dessau, Germany (1919-1925)",
-				"artist": "Walter Gropius",
-				"image": "url"
+				"artist": " Walter Gropius",
+				"image": "pics/Bauhause.jpg"
 			}, {
 				"title": "Composition with Large Red Plane, Yellow, Black, Gray, and Blue (1921)",
-				"artist": "Piet Mondrian",
-				"image": "url"
+				"artist": " Piet Mondrian",
+				"image": "pics/grid.jpg"
 			}]
 		}
 	}, {
@@ -48,12 +48,12 @@ const description =
 			"text": "",
 			"features": [{
 				"title": "The Accommodations of Desire (1929)",
-				"artist": "Salvador Dalí",
-				"image": "url"
+				"artist": " Salvador Dalí",
+				"image": "pics/The Accommodations of Desire.jpg"
 			}, {
-				"title": "",
-				"artist": "",
-				"image": "url"
+				"title": "The Palace at 4 a.m. (1932)",
+				"artist": " Alberto Giacometti",
+				"image": "pics/Alberto Giacometti.jpg"
 			}]
 		}
 	}, {
@@ -62,12 +62,12 @@ const description =
 			"text": "",
 			"features": [{
 				"title": "Lavender Mist(1950)",
-				"artist": "Jackson Pollock",
-				"image": "url"
+				"artist": " Jackson Pollock",
+				"image": "pics/number-1-lavender-mist(1).jpg"
 			}, {
 				"title": "1948 C (1948)",
-				"artist": "Clyfford Still",
-				"image": "url"
+				"artist": " Clyfford Still",
+				"image": "pics/1948 C.jpg"
 			}]
 		}
 	}, {
@@ -76,12 +76,12 @@ const description =
 			"text": "",
 			"features": [{
 				"title": "Campbell's Soup Cans (1962)",
-				"artist":"Andy Warhol",
-				"image": "url"
+				"artist":" Andy Warhol",
+				"image": "pics/warhol-MoMA-32-soup-cans.jpg"
 			}, {
 				"title": "Bunnies (1966)",
-				"artist": "Sigmar Polke",
-				"image": "url"
+				"artist": " Sigmar Polke",
+				"image": "pics/Bunnies (1966).jpg"
 			}]
 		}
 	}, {
@@ -90,12 +90,12 @@ const description =
 			"text": "",
 			"features": [{
 				"title": "Die (1962)",
-				"artist": "Tony Smith",
-				"image": "url"
+				"artist": " Tony Smith",
+				"image": "pics/Die (1962).jpg"
 			}, {
 				"title": "Die Fahne Hoch! (1959)",
-				"artist": "Frank Stella",
-				"image": "url"
+				"artist": " Frank Stella",
+				"image": "pics/Die Fahne Hoch!.jpg"
 			}]
 		}
 	}, {
@@ -104,12 +104,12 @@ const description =
 			"text": "",
 			"features": [{
 				"title": "McDonalds Pickup (1970)",
-				"artist": "Ralph Goings",
-				"image": "url"
+				"artist": " Ralph Goings",
+				"image": "pics/McDonalds Pickup (1970) .jpg"
 			}, {
 				"title": "The Woman Eating (1971)",
-				"artist":  "Duane Hanson",
-				"image": "url"
+				"artist":  " Duane Hanson",
+				"image": "pics/The Woman Eating (1971).jpg"
 			}]
 		}
 	}]
