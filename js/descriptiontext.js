82816fec10753c9c54f38bc066ffc4a48ef4b1b6
Yongjia Duan <yongjia@umich.edu>
2017-12-11 22:41:30 -0500

    text

--- js/descriptiontext.js
+++ js/descriptiontext.js
@@ -3,7 +3,7 @@ const description =
 	"data": [{
 		"information": {
 			"title": "Impressionism & Post-Impressionism (1865-1914)",
-			"text": "Impressionism can be considered the first distinctly modern movement in painting.",
+			"text": "Impressionism can be considered the first distinctly modern movement in painting. Developing in Paris in the 1860s, its influence spread throughout Europe and eventually the United States.",
 			"features": [{
 				"title": "Sunrise (1874)",
 				"artist": " Claude Monet",
@@ -17,7 +17,7 @@ const description =
 	}, {
 		"information": {
 			"title": "Cubism (1907-1922)",
-			"text":"Cubism is an early-20th-century art movement which brought European painting and sculpture historically forward toward 20th century Modern art.",
+			"text":"Cubism is an art movement in 1920s which brought European painting and sculpture historically forward toward 20th century Modern art. Cubism has been considered to be among the most influential art movements of the 20th century. The term is broadly used in association with a wide variety of art produced in Paris during the 1910s and extending through the 1920s. Representative art works of Cubism is characterized by the technique of representing simultaneity, multiple viewpoints.",
 			"features": [{
 				"title": "Les Desmoiselles d'Avignon (1907)",
 				"artist": ' Pablo Picasso',
@@ -31,7 +31,7 @@ const description =
 	}, {
 		"information": {
 			"title": 'Bauhaus (1919-1933)',
-			"text": '',
+			"text": "Bauhaus, was a German art school. The motivations behind the creation of the Bauhaus style lay in the 19th century, in anxieties about the soullessness of manufacturing and its products, and in fears about art's loss of purpose in society.  The Bauhaus had a major impact on art and architecture trends in Western Europe, the United States, Canada and Israel.",
 			"features": [{
 				"title": "Bauhaus building in Dessau, Germany (1919-1925)",
 				"artist": " Walter Gropius",
@@ -45,7 +45,7 @@ const description =
 	}, {
 		"information": {
 			"title": "Surrealism (1924-1942)",
-			"text": "",
+			"text": "Surrealism is an art movement originated in post-World War I Europe, lasted until late 1930s, is best known for its visual artworks and writings. During the World War II, a hugh ravage dramatically impacted the European artists that against Fascism and Nazism. Many important artists fled to North America and relative safety in the United States. Surrealism is also strongly impacted the art of Latin America. The French poet and founder of Surrealism, André Breton visited Mexico in 1938, he proclaimed it to be 'the surrealist country par excellence.' The central value of Surrealism in Latin America is contradiction which is embodied by the mestizo culture, the legacy of European conquer over native peoples.",
 			"features": [{
 				"title": "The Accommodations of Desire (1929)",
 				"artist": " Salvador Dalí",
@@ -59,7 +59,7 @@ const description =
 	}, {
 		"information": {
 			"title": "Abstract Expressionism (1940-1950)",
-			"text": "",
+			"text": "It was the first specifically American movement to achieve international influence and put New York City at the center of the western art world, a role formerly filled by Paris (Wikipedia). The shift was in large part due to both the art criticism and political environments of its time, but also collectors like Peggy Guggenheim, and curators like MoMA’s Alfred H Barr. It was called “Cold War art”. Abstraction had been allowed to thrive in part due to the earlier incredible amount of government funds being used to directly employ artists and commission new public artworks in the aftermath of the Great Depression.",
 			"features": [{
 				"title": "Lavender Mist(1950)",
 				"artist": " Jackson Pollock",
@@ -73,7 +73,7 @@ const description =
 	}, {
 		"information": {
 			"title": "Pop Art(1950-1970)",
-			"text": "",
+			"text": "Pop art is an art movement that originated in Britain and the United States in 1950s. The movement challenged the traditional forms of fine art and started to include imagery from mass culture, such as advertising, comic books and etc. Pop art often takes imagery that is currently in use in advertising. Product labeling and logos figure prominently in the imagery chosen by pop artists, seen in the labels of Campbell's Soup Cans, by Andy Warhol.Pop art became especially popular in Japan, evolved from the nation's prominent avant-garde scene. In the mid-1960s, graphic designer Tadanori Yokoo became one of the most successful pop artists and an international symbol for Japanese pop art.",
 			"features": [{
 				"title": "Campbell's Soup Cans (1962)",
 				"artist":" Andy Warhol",
@@ -87,7 +87,7 @@ const description =
 	}, {
 		"information": {
 			"title":"Minimalism(1960-1970)",
-			"text": "",
+			"text": "Minimalism is a style that uses pared-down design elements, it began in post–World War II Western art, most strongly with American visual arts in the 1960s and early 1970s. Minimalism in visual art, generally referred to as 'minimal art' emerged in New York in the early 1960s as new and older artists moved toward geometric abstraction. In Japan, the traditional culture of Zen Philosophy introduced the idea of simplicity in minimalism. Japanese manipulate the Zen culture into aesthetic and design elements for their buildings. This idea of architecture has influenced Western Society, especially in America since the mid 18th century.",
 			"features": [{
 				"title": "Die (1962)",
 				"artist": " Tony Smith",
@@ -101,7 +101,7 @@ const description =
 	}, {
 		"information": {
 			"title": "Photorealism(1970-2000)",
-			"text": "",
+			"text": "Photorealism were both reactionary movements stemming from the ever increasing and overwhelming abundance of photographic media, which by the mid 20th century had grown into such a massive phenomenon that it was threatening to lessen the value of imagery in art. The Photorealists attempts to reproduce the image as realistically as possible in another medium to reclaim and exalt the value of an image.",
 			"features": [{
 				"title": "McDonalds Pickup (1970)",
 				"artist": " Ralph Goings",
