c717f11310d8542a262b43fa6f78381e0d4d728b
garycheung13 <garyc102@gmail.com>
2018-01-25 20:46:48 -0500

    clean up files

--- js/descriptiontext.js
+++ js/descriptiontext.js
@@ -1,5 +1,4 @@
-const description =
-{
+const description = {
 	"data": [{
 		"information": {
 			"title": "Impressionism & Post-Impressionism (1865-1914)",
@@ -17,7 +16,7 @@ const description =
 	}, {
 		"information": {
 			"title": "Cubism (1907-1922)",
-			"text":"Cubism is an art movement in 1920s which brought European painting and sculpture historically forward toward 20th century Modern art. Cubism has been considered to be among the most influential art movements of the 20th century. The term is broadly used in association with a wide variety of art produced in Paris during the 1910s and extending through the 1920s. Representative art works of Cubism is characterized by the technique of representing simultaneity, multiple viewpoints.",
+			"text": "Cubism is an art movement in 1920s which brought European painting and sculpture historically forward toward 20th century Modern art. Cubism has been considered to be among the most influential art movements of the 20th century. The term is broadly used in association with a wide variety of art produced in Paris during the 1910s and extending through the 1920s. Representative art works of Cubism is characterized by the technique of representing simultaneity, multiple viewpoints.",
 			"features": [{
 				"title": "Les Desmoiselles d'Avignon (1907)",
 				"artist": ' Pablo Picasso',
@@ -76,7 +75,7 @@ const description =
 			"text": "Pop art is an art movement that originated in Britain and the United States in 1950s. The movement challenged the traditional forms of fine art and started to include imagery from mass culture, such as advertising, comic books and etc. Pop art often takes imagery that is currently in use in advertising. Product labeling and logos figure prominently in the imagery chosen by pop artists, seen in the labels of Campbell's Soup Cans, by Andy Warhol.Pop art became especially popular in Japan, evolved from the nation's prominent avant-garde scene. In the mid-1960s, graphic designer Tadanori Yokoo became one of the most successful pop artists and an international symbol for Japanese pop art.",
 			"features": [{
 				"title": "Campbell's Soup Cans (1962)",
-				"artist":" Andy Warhol",
+				"artist": " Andy Warhol",
 				"image": "pics/warhol-MoMA-32-soup-cans.jpg"
 			}, {
 				"title": "Bunnies (1966)",
@@ -86,7 +85,7 @@ const description =
 		}
 	}, {
 		"information": {
-			"title":"Minimalism(1960-1970)",
+			"title": "Minimalism(1960-1970)",
 			"text": "Minimalism is a style that uses pared-down design elements, it began in post–World War II Western art, most strongly with American visual arts in the 1960s and early 1970s. Minimalism in visual art, generally referred to as 'minimal art' emerged in New York in the early 1960s as new and older artists moved toward geometric abstraction. In Japan, the traditional culture of Zen Philosophy introduced the idea of simplicity in minimalism. Japanese manipulate the Zen culture into aesthetic and design elements for their buildings. This idea of architecture has influenced Western Society, especially in America since the mid 18th century.",
 			"features": [{
 				"title": "Die (1962)",
@@ -108,7 +107,7 @@ const description =
 				"image": "pics/McDonalds Pickup (1970) .jpg"
 			}, {
 				"title": "The Woman Eating (1971)",
-				"artist":  " Duane Hanson",
+				"artist": " Duane Hanson",
 				"image": "pics/The Woman Eating (1971).jpg"
 			}]
 		}
